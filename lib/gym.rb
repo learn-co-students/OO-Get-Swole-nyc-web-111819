62829db9bb2a220793fd23ed1ae7d49f90983ced
Shakil Abdin <shakil.abdin@gmail.com>
2019-11-23 18:57:02 -0500

    Done.

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,37 @@
 class Gym
   attr_reader :name
 
+  @@all = [] 
   def initialize(name)
     @name = name
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    # all memeberships at this gym
+    Membership.all.select {|memberships| memberships.gym == self}
+  end
+
+  def lifters
+    # all the lifters at a specific gym
+    memberships.map {|memberships| memberships.lifter}
+
+  end
+
+  def lifters_name
+    # name of all lifters that gym
+    lifters.map {|l| l.name}
+
+  end
+
+  def lift_total
+    # combing lift total of all members at that gym
+    lifters.map {|l| l.lift_total}.sum
+  end
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,37 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    # all the memberships the lifters have
+    Membership.all.select {|memberships| memberships.lifter == self}
+  end
+
+  def self.average_lift_total
+    # average lift total for all lifers
+    lift_total_array = all.map {|lifters| lifters.lift_total}
+    lift_total_array.sum / lift_total_array.length
+  end
+
+  def total_cost
+    # total money spent on gym memberships
+    memberships.map {|memberships| memberships.cost}.sum
+  end
+
+  def gym_signup (gym, cost)
+    # given gym and cost sign lifter up for new gym
+    Membership.new(self, gym, cost)
+  end
+
 end
