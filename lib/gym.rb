251b15b0ad84045bed92a30b1185724f8fa05f2f
github username <github email address>
2019-11-25 16:27:46 -0500

    finished gym lab

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,39 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    self.class.all << self 
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.gym == self 
+    end 
+  end 
+
+  def lifters 
+    memberships.map do |membership|
+      membership.lifter
+    end 
+  end 
+
+  def lifter_names
+    lifters.map do |lifter|
+      lifter.name 
+    end 
+  end 
+
+  def lifter_total 
+    lifters.reduce(0) do |total, lifter|
+      total + lifter.lift_total 
+    end 
+  end 
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,6 +2,14 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
+anna = Lifter.new("Anna",80)
+ethan = Lifter.new("Ethan", 90)
+equinox = Gym.new("Equinox")
+ny_sports_club = Gym.new("NY Sports Club")
+membership1 = Membership.new(200, anna, equinox)
+membership2 = Membership.new(200, ethan, equinox)
+membership3 = Membership.new(50, anna, ny_sports_club)
+
 # test code goes here
 
 binding.pry
